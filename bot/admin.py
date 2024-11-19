1713405fb0a9cc412f9f1d1685b5c3cb3d3fad98
b.kubulbekov <b.kubulbekov@tanto-s.ru>
2024-11-19 16:42:47 +0600

    admin

--- bot/admin.py
+++ bot/admin.py
@@ -4,6 +4,5 @@ from bot.models import TelegramUser, Code, Video, FAQ, Category
 
 admin.site.register(TelegramUser)
 admin.site.register(Code)
-admin.site.register(Video)
 admin.site.register(FAQ)
 admin.site.register(Category)
